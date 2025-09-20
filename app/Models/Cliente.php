43961f3308cec6f78fe80bd9bed1ad519cd3e7a9
Abigail <arcanivanessa38@gmail.com>
2025-09-20 02:42:51 -0400

    SE AGREGO LA MIGRACION DE clientes Y EL MODELO DE cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Cliente extends Model
 {
-    
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'clientes';
+
+    protected $fillable = [
+        'usuario_creador_id',
+        'usuario_modificador_id',
+        'usuario_eliminador_id',
+
+        'nombres',
+        'ap_paterno',
+        'ap_materno',
+        'celular',
+        'cedula',
+        'nit',
+        'razon_social',
+
+        'estado',
+        'deleted_at',
+    ];
+
+
 }
